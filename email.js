7f22c365be9bb6cca8f1f92b5744d6a4655fc2dc
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2021-08-08 12:10:36 +0200

    email validation

---
+++ email.js
@@ -0,0 +1,10 @@
+// Simplified email validation
+
+function validateEmail(email) {
+    const regex = /\S+@\S+\.\S+/.test(email)
+    return regex;
+}
+
+//  \S+@        # check if there is non-whitespace character before @
+//  \S+\.       # check if there is non-whitespace character between @ and .
+//  \S+         # check if there is non-whitesoace character after .
